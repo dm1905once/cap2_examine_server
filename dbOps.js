34ea58e1e17300397f33c7f40446561ede72d595
danielm <dm1905once@gmail.com>
2020-09-27 20:05:52 -0700

    Move Examiner DB ops to its own class

---
+++ dbOps.js
@@ -0,0 +1,67 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+class Examiner {
+
+    static async getAll(){
+        const examiners = await prisma.examiners.findMany();
+        return examiners;
+    }
+
+    static async exists(username){
+        const usernameCount = await prisma.examiners.count({
+            where: {username: username}
+        });
+        return (usernameCount>0)? true: false;
+    }
+
+    static async register(formData, hashedPassword){
+        const { username, email, first_name, last_name, org_handle, photo_url} = formData;
+        const newExaminer = await prisma.examiners.create({ 
+            data: {
+                username, 
+                email, 
+                first_name, 
+                last_name, 
+                password: hashedPassword, 
+                photo_url,
+                organizations: {
+                    connect: { handle: org_handle}
+                }
+            },
+            select : {
+                username: true,
+                organizations: {
+                    select :{
+                        name: true,
+                        handle: true,
+                        logo_url: true
+                    }
+                }
+            }
+        });
+        return newExaminer;
+    }
+
+    static async authenticate(username){
+        const result = await prisma.examiners.findMany({ 
+            where: { username },
+            select : {
+                username: true,
+                password: true,
+                organizations: {
+                    select :{
+                        name: true,
+                        handle: true,
+                        logo_url: true
+                    }
+                }
+            }
+        });
+        return result;
+    }
+}
+
+module.exports = {
+    Examiner
+  };
--- routes/examiners.js
+++ routes/examiners.js
@@ -2,17 +2,16 @@
 
 const express = require("express");
 const bcrypt = require("bcryptjs");
-const { createExaminerToken } = require('../helpers/createToken');
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
 const router = new express.Router();
+const { createExaminerToken } = require('../helpers/createToken');
 const { authRequired, ensureCorrectUser } = require("../middleware/auth");
+const { Examiner } = require('../dbOps');
 
 const BCRYPT_WORK_FACTOR = 10;
 
-router.get("/", async function (req, res, next) {
+router.get("/", authRequired, async function (req, res, next) {
     try {
-        const allExaminers = await prisma.examiners.findMany();
+        const allExaminers = Examiner.getAll();
         res.json(allExaminers);
     }
     catch (err) {
@@ -21,40 +20,14 @@ router.get("/", async function (req, res, next) {
 });
 
 router.post("/register", async function (req, res, next) {
-    const { username, email, first_name, last_name, password, org_handle, photo_url} = req.body;
+    const { username,  password } = req.body;
     try{
-        const duplicateUsername = await prisma.examiners.count({
-            where: {username: username}
-        });
-
-        if (duplicateUsername > 0 ) {
+        if (await Examiner.exists(username)) {
             const err = new Error(`username '${username} already exists`);
             err.status = 409; throw err;
         } else {
             const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
-            const newExaminer = await prisma.examiners.create({ 
-                data: {
-                    username, 
-                    email, 
-                    first_name, 
-                    last_name, 
-                    password: hashedPassword, 
-                    photo_url,
-                    organizations: {
-                        connect: { handle: org_handle}
-                    }
-                },
-                select : {
-                    username: true,
-                    organizations: {
-                        select :{
-                            name: true,
-                            handle: true,
-                            logo_url: true
-                        }
-                    }
-                }
-            });
+            const newExaminer = await Examiner.register(req.body, hashedPassword);
             const token = createExaminerToken(newExaminer);
             return res.status(201).json({ token });
         }
@@ -68,20 +41,7 @@ router.post("/register", async function (req, res, next) {
 router.post("/login", async function (req, res, next) {
     const { username, password } = req.body;
     try {
-        const result = await prisma.examiners.findMany({ 
-            where: { username },
-            select : {
-                username: true,
-                password: true,
-                organizations: {
-                    select :{
-                        name: true,
-                        handle: true,
-                        logo_url: true
-                    }
-                }
-            }
-        });
+        const result = await Examiner.authenticate(username);
         const examiner = result[0];
         if (examiner) {
             const isValid = await bcrypt.compare(password, examiner.password);
@@ -91,7 +51,6 @@ router.post("/login", async function (req, res, next) {
             }
         }
 
-        // Default action if examiner was not found or credentials were invalid
         const invalidPass = new Error("Invalid Credentials");
         invalidPass.status = 401;
         throw invalidPass;
