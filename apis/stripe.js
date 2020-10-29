2face1e4f7d20e3bece57805d7b2d8489e25c5d2
danielm <dm1905once@gmail.com>
2020-10-28 18:06:39 -0700

    Validate that images element is removed if empty when sent to stripe

--- apis/stripe.js
+++ apis/stripe.js
@@ -1,28 +1,34 @@
 const { CLIENT_REDIRECT_URL, STRIPE_SECRET } = require('../config');
 const stripe = require('stripe')(STRIPE_SECRET);
 
-async function createStripeSession(examDetails, exam_id, application_id, applicant_email, org_logo){
-    const session = await stripe.checkout.sessions.create({
-        payment_method_types: ['card'],
-        line_items: [
-          {
-            price_data: {
-              currency: 'usd',
-              product_data: {
-                name: examDetails.exam_name,
-                images: [org_logo],
-              },
-              unit_amount: parseInt(examDetails.exam_fee)*100,
-            },
-            quantity: 1,
-          },
-        ],
-        mode: 'payment',
-        success_url: `${CLIENT_REDIRECT_URL}/applicants?success=true&application_id=${application_id}&exam_id=${exam_id}&applicant_email=${applicant_email}`,
-        cancel_url: `${CLIENT_REDIRECT_URL}/applicants?canceled=true`,
-      });
+async function createStripeSession(examDetails, exam_id, application_id, applicant_email, org_logo=''){
 
-      return { id: session.id };
+  const productInfo = {
+    payment_method_types: ['card'],
+    line_items: [
+      {
+        price_data: {
+          currency: 'usd',
+          product_data: {
+            name: examDetails.exam_name,
+            images: [org_logo],
+          },
+          unit_amount: parseInt(examDetails.exam_fee)*100,
+        },
+        quantity: 1,
+      },
+    ],
+    mode: 'payment',
+    success_url: `${CLIENT_REDIRECT_URL}/applicants?success=true&application_id=${application_id}&exam_id=${exam_id}&applicant_email=${applicant_email}`,
+    cancel_url: `${CLIENT_REDIRECT_URL}/applicants?canceled=true`,
+  };
+
+  if (org_logo === '') {
+    delete productInfo.line_items[0].price_data.product_data.images;
+  };
+
+  const session = await stripe.checkout.sessions.create(productInfo);
+  return { id: session.id };
 }
 
 module.exports = {
