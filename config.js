f676fda57f373af3d57c86b074e8f141693dc771
danielm <dm1905once@gmail.com>
2020-11-03 20:11:30 -0800

    Updated secret stripe key

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 const CLIENT_REDIRECT_URL = process.env.LOCALHOST || 'http://localhost:3000';
 const SECRET = process.env.SECRET_KEY || 'test123';
-const STRIPE_SECRET = process.env.STRIPE_SECRET || 'sk_test_51HcDauJMaHZnra3giyyZRYTwmYjCgkGmx1Ym9DvPXHtpFj3zEYQZXUNtp2OEBDwxcsHHbxwp81iUuCfSmgSAEENT00WHuvBPlM';
+const STRIPE_SECRET = process.env.STRIPE_SECRET || 'sk_test_51HcDauJMaHZnra3gx1nCAIOtlE0laOp8v4SrED5uEM3LKnHbisV0AgjRqV5NyrMh4EgT809zM8yo1H0AqEVl7qZW00HNIR6huL';
 const PORT = process.env.PORT || 3001;
 
 let DB_URI;
